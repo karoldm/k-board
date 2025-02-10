c0030f164fddbafe572d29cf01605f32a59b1054
karol marques <karoldm1579@gmail.com>
2025-02-10 17:02:34 -0300

    fix: remove test comment

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export const App = () => {
   const { isAuth } = useUser();
 
   function ProtectedRoute({ authenticationPath, outlet }: ProtectedRouteProps) {
-    if (/*isAuth()*/ true) {
+    if (isAuth()) {
       return outlet;
     } else {
       return <Navigate to={{ pathname: authenticationPath }} />;
