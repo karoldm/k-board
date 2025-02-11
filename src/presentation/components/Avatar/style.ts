872918c864744d87c9d336db28544a839eb560da
karol marques <karoldm1579@gmail.com>
2025-02-11 16:04:45 -0300

    feat: default avatar when src is empty

--- src/presentation/components/Avatar/style.ts
+++ src/presentation/components/Avatar/style.ts
@@ -1,15 +1,17 @@
-import styled from 'styled-components';
-
+import styled from 'styled-components'
 
 type Props = {
-  src?: string;
-  width?: string;
+  src?: string
+  width?: string
 }
 
 export const StyledAvatar = styled.img<Props>`
   border-radius: 100%;
-  width: ${props => props.width ?? "40px"};
-  height: ${props => props.width ?? "40px"};
+  width: ${(props) => props.width ?? '40px'};
+  height: ${(props) => props.width ?? '40px'};
   background: var(--primary);
   border: 2px solid var(--white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
