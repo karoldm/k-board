73b55ba46d4f2deedc38d08f6f66fa4f2994dd01
karol marques <karoldm1579@gmail.com>
2025-02-12 18:40:03 -0300

    fix: initial value to edit project modal

--- src/presentation/components/Modal/NewProjectModal/index.tsx
+++ src/presentation/components/Modal/NewProjectModal/index.tsx
@@ -1,17 +1,16 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react"
-import { FieldValues, useForm } from "react-hook-form";
-import { projectSchema } from "../../../schemas/project.schema";
-import { Button } from "../../Button";
-import { Input } from "../../Input"
-import { Column } from "../../Layouts/Column";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FieldValues, useForm } from 'react-hook-form'
+import { projectSchema } from '../../../schemas/project.schema'
+import { Button } from '../../Button'
+import { Input } from '../../Input'
+import { Column } from '../../Layouts/Column'
 
 type Props = {
-  initialValue?: string;
-  handleConfirm: (title: string) => void;
+  initialValue?: string
+  handleConfirm: (title: string) => void
 }
 
-export const NewProjectModal = ({handleConfirm, initialValue}: Props) => {
+export const NewProjectModal = ({ handleConfirm, initialValue }: Props) => {
   const {
     register,
     handleSubmit,
@@ -19,22 +18,29 @@ export const NewProjectModal = ({handleConfirm, initialValue}: Props) => {
     formState: { errors },
   } = useForm({
     resolver: zodResolver(projectSchema),
-  });
+  })
 
   const handleSave = async (data: FieldValues) => {
-    handleConfirm(data.title);
-    console.log(data);
-    reset();
+    handleConfirm(data.title)
+    console.log(data)
+    reset()
   }
 
   return (
-    <Column as="form" onSubmit={handleSubmit((data)=>handleSave(data))} gap="8px">
+    <Column
+      as='form'
+      onSubmit={handleSubmit((data) => handleSave(data))}
+      gap='8px'
+    >
       <Input
+        defaultValue={initialValue}
         placeholder='Título'
         error={errors.title?.message?.toString()}
         {...register('title')}
       />
-      <Button type="submit" ><p>Salvar</p></Button>
+      <Button type='submit'>
+        <p>Salvar</p>
+      </Button>
     </Column>
-  );
+  )
 }
