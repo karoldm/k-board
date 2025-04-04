0c19a1348499e2b8ebba66c6d60ad64da701af43
karolyne marques <karoldm1579@gmail.com>
2025-04-04 18:36:26 -0300

    add password check on login schema

--- src/presentation/schemas/login.schema.ts
+++ src/presentation/schemas/login.schema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email("Esse email não é válido"),
-  password: z.string().min(1, {message: "A senha deve ter pelo menos 1 caractere"}),
+  password: z.string().min(8, {message: "A senha deve ter pelo menos 8 caractere"}),
 });
